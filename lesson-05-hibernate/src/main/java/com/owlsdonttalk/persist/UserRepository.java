327942fbaeeefa44cc85acae6dacc2b6bafb4115
Ivan Petrov <petrovodt@gmail.com>
2021-02-16 20:37:02 +0300

    l6 init

--- lesson-05-hibernate/src/main/java/com/owlsdonttalk/persist/UserRepository.java
+++ lesson-05-hibernate/src/main/java/com/owlsdonttalk/persist/UserRepository.java
@@ -2,60 +2,71 @@ package com.owlsdonttalk.persist;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class UserRepository {
 
     private final EntityManagerFactory emFactory;
-    private EntityManager em;
 
     public UserRepository(EntityManagerFactory emFactory) {
         this.emFactory = emFactory;
-        this.em = emFactory.createEntityManager();
-        em.getTransaction().begin();
-        init();
-    }
-
-    private void init(){
-        User u1 = new User("name1");
-        User u2 = new User("name2");
-        User u3 = new User("name3");
-
-        em.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
-        em.persist(u1);
-        em.persist(u2);
-        em.persist(u3);
-        em.getTransaction().commit();
-
     }
 
     public List<User> findAll() {
-        List<User> userList = em.createQuery("from User", User.class)
-                .getResultList();
-
-        return userList;
+        return executeForEntityManager(
+                em -> em.createNamedQuery("allUsers", User.class).getResultList()
+        );
     }
 
-    public User findById(Long id) {
-        return em.find(User.class, id);
+    public User findById(long id) {
+        return executeForEntityManager(
+                em -> em.find(User.class, id)
+        );
     }
 
     public void insert(User user) {
-        em.getTransaction().begin();
-        em.persist(user);
-        em.getTransaction().commit();
+        executeInTransaction(em -> em.persist(user));
     }
 
     public void update(User user) {
-        em.getTransaction().begin();
-        em.merge(user);
-        em.getTransaction().commit();
+        executeInTransaction(em -> em.merge(user));
     }
 
     public void delete(long id) {
-        em.getTransaction().begin();
-        em.remove(findById(id));
-        em.getTransaction().commit();
+        executeInTransaction(em -> {
+            User user = em.find(User.class, id);
+            if (user != null) {
+                em.remove(user);
+            }
+        });
     }
+
+    private <R> R executeForEntityManager(Function<EntityManager, R> function) {
+        EntityManager em = emFactory.createEntityManager();
+        try {
+            return function.apply(em);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    private void executeInTransaction(Consumer<EntityManager> consumer) {
+        EntityManager em = emFactory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            consumer.accept(em);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            em.getTransaction().rollback();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
 }
