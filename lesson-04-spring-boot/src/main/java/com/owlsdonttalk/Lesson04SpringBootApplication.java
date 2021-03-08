05cbe22ad6e2ce93a1e000ffdeee8a458d89ea3b
Ivan Petrov <petrovodt@gmail.com>
2021-03-08 04:53:47 +0300

    l10 init

--- lesson-04-spring-boot/src/main/java/com/owlsdonttalk/Lesson04SpringBootApplication.java
+++ lesson-04-spring-boot/src/main/java/com/owlsdonttalk/Lesson04SpringBootApplication.java
@@ -3,6 +3,10 @@ package com.owlsdonttalk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// http://localhost:8080/spring-mvc-app/swagger-ui/index.html
+// http://localhost:8080/spring-mvc-app/v3/api-docs/
+// https://www.baeldung.com/spring-rest-openapi-documentation
 @SpringBootApplication
 public class Lesson04SpringBootApplication {
 
